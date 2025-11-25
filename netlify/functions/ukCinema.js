1d266ae2ccd60c323b9edcc340617d7e726c4073
p4uli0 <paul.graham111@icloud.com>
2025-11-25 15:10:58 +0000

    Update ukCinema.js

--- netlify/functions/ukCinema.js
+++ netlify/functions/ukCinema.js
@@ -1,10 +1,14 @@
+// netlify/functions/ukCinema.js
+
 export async function handler(event) {
+  const API_KEY = process.env.UK_CINEMA_API_KEY;
   const url = "https://uk-cinema-api.co.uk/api/v2/showtimes?page=1&items=20";
 
   try {
     const response = await fetch(url, {
       headers: {
         Accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`, // IMPORTANT
       },
     });
 
